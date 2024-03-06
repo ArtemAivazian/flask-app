8e5cf2d27446442e1d1a05f3b2d05ed1be199edc
Aivazian, Artem <aivazart@fel.cvut.cz>
2024-03-06 23:02:07 +0100

    bug fixes

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)  # Vytvoření instance třídy Flask
 
+# Template data
+measurements = [
+    {"timestamp": "2024-03-03 12:00", "temp": 20.5},
+    {"timestamp": "2024-03-03 12:30", "temp": 21.0},
+    {"timestamp": "2024-03-03 13:00", "temp": 20.8},
+    {"timestamp": "2024-03-03 13:30", "temp": 21.2},
+    {"timestamp": "2024-03-03 14:00", "temp": 20.9},
+]
+
 # View funkce pro endpoint '/login' etc. namapovani funkce login() na dir /login
 @app.route('/login')
 def login():
@@ -12,8 +21,10 @@ def register():
     return render_template("register.html")
 
 @app.route('/dashboard')
-def register():
-    return render_template("dashboard.html")
+def dashboard():
+    # for example
+    username = "aivazart@fel.cvut.cz"
+    return render_template("dashboard.html", username=username, measurements=measurements)
 
 # Spuštění webové aplikace na adrese 0.0.0.0:3000 v debug režimu
 if __name__ == '__main__':
